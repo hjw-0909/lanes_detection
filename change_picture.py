63c57431a3c579db56ddad637245875eddb74810
jiawei <hjw1230909@163.com>
2022-12-15 20:34:53 +0800

    first

---
+++ change_picture.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import cv2 as cv
+import os
+
+for filename_1 in os.listdir('work_test_sim_shape'):
+    for filename_2 in os.listdir('work_test_2'):
+        if filename_1 == filename_2:
+            img_1 = cv.imread('work_test_sim_shape' + '/' + filename_1)
+            img_2 = cv.imread('work_test_2' + '/' + filename_2)
+            img = cv.vconcat([img_1, img_2])
+            img_name = "picture_loss/" + str(filename_1)
+            cv.imwrite(img_name, img)
+            break
+
+path = 'picture_loss'
+nums = len(os.listdir(path))
+print(nums)
