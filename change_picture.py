b9907a64bcb59fd356d679cd2f4b17d6618591d5
jiawei <hjw1230909@163.com>
2022-12-23 12:37:26 +0800

    second

--- change_picture.py
+++ change_picture.py
@@ -1,17 +1,19 @@
 # -*- coding:utf-8 -*-
 import cv2 as cv
 import os
-
-for filename_1 in os.listdir('work_test_sim_shape'):
+i = 1
+for filename_1 in os.listdir('work_test_sim_r1'):
     for filename_2 in os.listdir('work_test_2'):
         if filename_1 == filename_2:
-            img_1 = cv.imread('work_test_sim_shape' + '/' + filename_1)
+            img_1 = cv.imread('work_test_sim_r1' + '/' + filename_1)
             img_2 = cv.imread('work_test_2' + '/' + filename_2)
             img = cv.vconcat([img_1, img_2])
-            img_name = "picture_loss/" + str(filename_1)
+            img_name = "picture_com/" + str(filename_1)
             cv.imwrite(img_name, img)
+            print(str(i) + ' images have been processed!')
+            i += 1
             break
 
-path = 'picture_loss'
+path = 'picture_com'
 nums = len(os.listdir(path))
 print(nums)
